a508ab34ae5ad89a697b483d8a293f04c71ef2b1
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-17 04:13:08 -0400

    d

---
+++ VS HTML ASSINGMENT SUBMISSION/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 150;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 800) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+        }
+    }
+}
